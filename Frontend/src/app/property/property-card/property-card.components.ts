80d0a10aa52be56cd5b72438d3d6d4235c2d2330
unteo <unteo08@gmail.com>
2025-06-05 12:06:09 +0300

    Create Property list Pasing data between components Using ngFor Directive Using Property Binding

--- Frontend/src/app/property/property-card/property-card.components.ts
+++ Frontend/src/app/property/property-card/property-card.components.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-property-card',
@@ -8,11 +8,5 @@ import { Component } from "@angular/core";
     styleUrls: ['property-card.components.css']
 })
 export class PropertyCardComponent{
-    Property: any = {
-        "Id":1,
-        "Name":"Birla House",
-        "Type":"House",
-        "Price":12000,
-    }
-
+ @Input() property : any
 }
