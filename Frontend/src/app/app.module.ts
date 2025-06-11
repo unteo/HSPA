98d26249cb550b199cabff8c9171016c3117f999
unteo <unteo08@gmail.com>
2025-06-11 11:57:08 +0300

    Understanding routing part 1 & 2

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.components';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from 'src/services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
+const appRoutes: Routes =[
+ { path: '', component : PropertyListComponent },
+ { path: 'rent-property', component : PropertyListComponent },
+ { path: 'add-property', component : AddPropertyComponent },
+ { path: 'property-detail/:id', component : PropertyDetailComponent },
+
+]
 
 @NgModule({
   declarations: [	
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent,
       NavBarComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService,
