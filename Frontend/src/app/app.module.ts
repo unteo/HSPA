86277810e3997a3996e5ead5843e503caed7c099
unteo <unteo08@gmail.com>
2025-06-13 17:15:02 +0300

    Reactive forms

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.components';
@@ -11,12 +11,16 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from 'src/services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const appRoutes: Routes =[
  { path: '', component : PropertyListComponent },
  { path: 'rent-property', component : PropertyListComponent },
  { path: 'add-property', component : AddPropertyComponent },
  { path: 'property-detail/:id', component : PropertyDetailComponent },
+ { path: 'user/login', component: UserLoginComponent},
+ { path: 'user/register', component: UserRegisterComponent},
  { path: '**', component : PropertyListComponent },
 
 
@@ -27,14 +31,17 @@ const appRoutes: Routes =[
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
+    NavBarComponent,
     AddPropertyComponent,
     PropertyDetailComponent,
-      NavBarComponent
+    UserRegisterComponent,
+    UserLoginComponent,
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
